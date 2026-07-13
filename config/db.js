850baf408e8b1e2dab3f7f36f821d9ed62d4bf8b
niharikagolthi24-commits <niharika.golthi24@sasi.ac.in>
2026-07-13 14:44:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dns = require("dns") 
+dns.setServers(["8.8.8.8",8.8.4.4'])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ContactManager");
+        await mongoose.connect("mongodb+srv://niharikagolthi:<db_password>@cluster0.0ys5bsa.mongodb.net/?appName=Cluster0");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
